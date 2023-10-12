6429bd0a134dfa9e1ea119c0a19e57ca2f786e74
haricypress <pirate.evil.war@gmail.com>
2023-10-12 23:11:20 +0530

    s

---
+++ JavaScript/countWords.js
@@ -0,0 +1,17 @@
+
+// count specific word(s) in entire text
+
+text = "str string str1,str2,str3,i am am am amama str4"
+
+search_text = "str"
+console.log(search_text,' : ',
+(text.match(new RegExp(search_text, "g")) || []).length)
+
+search_text = "am" // without space
+console.log(search_text,' : ',
+(text.match(new RegExp(search_text, "g")) || []).length)
+
+
+search_text = "am "  // with space
+console.log(search_text,' : ',
+(text.match(new RegExp(search_text, "g")) || []).length)
