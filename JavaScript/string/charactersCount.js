3dbaaaf27da16e03240c1205576f7bdeda6f8173
haricypress <pirate.evil.war@gmail.com>
2023-10-16 21:49:52 +0530

    s

--- JavaScript/string/charactersCount.js
+++ JavaScript/string/charactersCount.js
@@ -1,68 +1,82 @@
+console.log("false + 1+2  :    ",false+1+2)
+console.log("undefined + 1+2   :   ", undefined+1+2)
 
-// 1. counting everyting include special characters
+console.log("undefined + false + 1+2   :   ", (undefined || false)+1+2)
 
-function countCharacters(str) {
-  const charCount = {};
-  for (let char of str) {
+No = (undefined || false)+1
+console.log(No)
+No = No +9
+console.log(No)
 
-    // console.log("before:",charCount,char,charCount[char])
-    charCount[char] = (charCount[char] || false) + 1;
-    // console.log("after:",charCount,char, charCount[char],"\n===========")
-  }
 
-  return charCount;
-}
-string = "i am an indian, 111111112";
-result = countCharacters(string);
-//  to print new line  "\n"  used
-console.log("\nstring :", string);
-console.log("all characters count :\n", result, "\n");
-console.log("=======================================\n")
 
-//===========================================================================================
+// // 1. counting everyting include special characters
 
-/*
-note :
+// function countCharacters(str) {
 
-      first time letterCount[char] not available, at that time "false"  available,
-      that's why "false" added in below statement.
-      letterCount[char] = (letterCount[char] || false) + 1
-      after that, count value increasing by 1.
+//   // "charCount" is object, looks like,   charCount  = { "i" : 1, "am" : 3 }
+//   const charCount = {}; 
+//   for (let char of str) {
+
+//     console.log("before:",charCount,char,charCount[char])
+//     charCount[char] = (charCount[char] || false) + 1;
+//     console.log("after:",charCount,char, charCount[char],"\n===========")
+//   }
+
+//   return charCount;
+// }
+// string = "i am an indian, 111111112";
+// result = countCharacters(string);
+// //  to print new line  "\n"  used
+// console.log("\nstring :", string);
+// console.log("all characters count :\n", result, "\n");
+// console.log("=======================================\n")
+
+// //===========================================================================================
+
+// /*
+// note :
+
+//       first time letterCount[char] not available, at that time "false"  available,
+//       that's why "false" added in below statement.
+//       letterCount[char] = (letterCount[char] || false) + 1
+//       after that, count value increasing by 1.
       
-*/
+// */
 
-//===========================================================================================
+// //===========================================================================================
 
-// 2. counting only   a to z, A to Z, 0 to 9   values
-
-
-function countLetters(str) {
-  var letterCount = {};
-
-  // Iterate through each character in the string
-  for (var i = 0; i < str.length; i++) {
-    var char = str[i];
-    // Check if the character is a letter
-    if (/[a-zA-Z0-9]/.test(char)) {
-
-      if (letterCount[char]) {
-        letterCount[char]++;
-      } else {
-        letterCount[char] = 1;
-      }
-
-      // or
-      // letterCount[char] = (letterCount[char] || false) + 1;    
-    }
-  }
-
-  return letterCount;
-}
+// // 2. counting only   a to z, A to Z, 0 to 9   values
 
 
-string = "i am an indian, 111111112";
-result = countLetters(string);
-console.log("String :",string);
-console.log("a-z  /  A-Z   /   0-9 character count :\n",result,"\n");
+// function countLetters(str) {
+//   var letterCount = {};
 
-//===========================================================================================
+//   // Iterate through each character in the string
+//   for (var i = 0; i < str.length; i++) {
+//     var char = str[i];
+//     // Check if the character is a letter
+//     if (/[a-zA-Z0-9]/.test(char)) {
+
+//       if (letterCount[char]) {
+//         letterCount[char]++;
+//       } else {
+//         letterCount[char] = 1;
+//       }
+
+//       // or
+//       // letterCount[char] = (letterCount[char] || false) + 1;    
+//     }
+//   }
+
+//   return letterCount;
+// }
+
+
+// string = "i am an indian, 111111112";
+// result = countLetters(string);
+// console.log("String :",string);
+// console.log("a-z  /  A-Z   /   0-9 character count :\n",result,"\n");
+
+// //===========================================================================================
+
