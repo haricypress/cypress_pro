f11c11ba78123e742d3dc208180bf49361a2de48
haricypress <pirate.evil.war@gmail.com>
2023-10-28 20:47:51 +0530

    ddd

---
+++ cypress/e2e/18_fixtures/resr.cy.js
@@ -0,0 +1,100 @@
+
+// in 4th test-case Error is there - while "json" file writing last "," need remove
+
+describe('verifying - if login manually logout, else check url', () => {
+
+    it.only('3. verify single correct credential and multiple wrong credentials', () => {
+
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            return false
+        })
+
+        // cy.fixture('login.json').then((data) => {
+
+        //     data.forEach((userdata) => {
+
+
+
+
+
+
+
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+        cy.get('input[name="username"]').type("Admjin")  // wrong username
+        cy.get('input[type="password"]').type("admin123")
+        cy.get('button[type="submit"]').click()
+
+        try {
+            if (cy.get('p.oxd-userdropdown-name').length() > 0)
+
+                cy.log("pass")
+            else
+                cy.log("fail")
+        }
+
+        catch (AssertionError) {
+
+            cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+        // cy.get('body').then(($body1) => {
+
+        //     try {
+
+        //         if ($body1.find('p.oxd-userdropdown-name').length > 0)
+
+        //             cy.log("pass")
+        //         else
+        //             cy.log("fail")
+        //     }
+        //     catch {
+
+        //     }
+        // })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // // if login
+        // cy.get('p.oxd-userdropdown-name').click()
+        // cy.get('a[role="menuitem"]').eq(3).click();
+        // cy.url().should('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+
+
+        // // if not login
+        // cy.contains("Login").should("be.visible")
+
+
+
+        //     })// forEach
+        // })// fixtures
+
+    })// it ==================================================================================
+
+})// describe
+
+
