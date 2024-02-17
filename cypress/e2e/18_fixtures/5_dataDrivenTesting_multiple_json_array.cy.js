7396769ffdd5f4694913ca4225b8f33bcd1f9d37
haricypress <pirate.evil.war@gmail.com>
2024-02-17 17:33:32 +0530

    updated

--- cypress/e2e/18_fixtures/5_dataDrivenTesting_multiple_json_array.cy.js
+++ cypress/e2e/18_fixtures/5_dataDrivenTesting_multiple_json_array.cy.js
@@ -6,8 +6,8 @@ const fixtureVariable = [
         'context': 'testData2'
     },
     {
-        "name": "testData3",
-        'context': 'testData3'
+        "name": "arrayJSON",
+        'context': 'arrayJSON'
     }
 ]
 
@@ -16,7 +16,7 @@ const fixtureVariable = [
 describe("load test data from multiple files demo", function () {
 
     fixtureVariable.forEach((vFixture) => {
-      
+
         describe(vFixture.context, () => {
             before(function () {  // hook  -  "before()"
 
@@ -28,16 +28,24 @@ describe("load test data from multiple files demo", function () {
             it("verify already registered user try to register again", function () {
 
 
-                cy.visit("https://shop.demoqa.com/my-account/")
-                cy.get("#reg_username").type(this.data.userName)
-         
-                cy.get("#reg_email").type(this.data.EmailAddr)
-                 
-                cy.get("#reg_password").type(this.data.strongPassword)
-                cy.get('button[name="register"]').click()
+                // // non-array kind JSON data
+                cy.log(this.data.userName)
+
+                // array kind JSON data
+                for (let i of this.data) {
+                    cy.log(i.userName)
+                }
+
+                // cy.visit("https://shop.demoqa.com/my-account/")
+                // cy.get("#reg_username").type(this.data.userName)
+
+                // cy.get("#reg_email").type(this.data.EmailAddr)
+
+                // cy.get("#reg_password").type(this.data.strongPassword)
+                // cy.get('button[name="register"]').click()
 
 
-                cy.get("#username").should("have.value", this.data.userName)
+                // cy.get("#username").should("have.value", this.data.userName)
 
 
             })
