40032a2415dfd92c20440dc006e577ca1f8459f6
haricypress <pirate.evil.war@gmail.com>
2024-07-18 14:23:01 +0530

    uodated scripts

--- JavaScript/largestFourNoArray.js
+++ JavaScript/largestFourNoArray.js
@@ -1,13 +1,20 @@
 
-arr = [0, 2, 3, 4, 5, 6, 7]
+/*
+     sort then add elements last 4 values, if length is > 4
+     sort then add 1st 4 elements if length is <= 4
+*/
+
+arr = [0, 1, 2, 1,1,1,1,]
+// arr = [0, 1, 2,]
+
 arr = arr.sort()
-console.log(arr)
+console.log("array : ",arr)
 
 length = arr.length
 last_index = length - 1
 let sum = 0
 
-if (length >= 4) {
+if (length > 4) {
     let four_val = ""
 
     for (let i = last_index; i >= last_index - 3; i--) {
@@ -15,13 +22,14 @@ if (length >= 4) {
         four_val += arr[i] + " "
         sum += arr[i]
     }
-    console.log(four_val, sum)
+    console.log("last 4 values : ",four_val)
+    console.log("adding elements from 5th element :", sum)
 }
 else {
     for (let i = 0; i < length; i++) {
         sum += arr[i]
     }
-    console.log(sum)
+    console.log("adding 1st 4 elements :",sum)
 }
 
 
